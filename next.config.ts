f9c82aa5f6f6f6d7bb0072172f12299b81353398
Harshith-Daraboina <23bcs037@iiitdwd.ac.in>
2025-07-18 18:00:11 +0530

    init

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,15 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+    experimental: {
+    optimizeCss: false, // Disable Lightning CSS
+  },
   typescript: {
     ignoreBuildErrors: true, // Also ignore TypeScript errors if needed
   },
 }
 
 module.exports = nextConfig
+
+
+
